ac03be6b8f4d099b9b1a10561485d46c7b513167
sinan özlen <sinanozlen1@gmail.com>
2024-12-09 00:13:39 +0300

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,46 @@
-import { Routes } from '@angular/router';
+import { Route, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Component } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LayoutsComponent } from './layouts/layouts.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+//Route[] => Routes'a tanimlidir bu yuzden calisir.
+export const homeRoute:Route ={
+    path:"",
+    component:HomeComponent
+}
+
+
+export const routes: Routes = [
+    {
+        path:"login",
+        component:LoginComponent
+    },
+{
+    path:"",
+    component:LayoutsComponent,
+    children: [
+
+        homeRoute,
+        {
+            path: "",
+            component: HomeComponent
+        },
+        {
+            path: "home",
+            component: HomeComponent
+        },
+        {
+            path: "about",
+            component: AboutComponent
+        },
+        {
+            path: "contact",
+            component: ContactComponent
+        }
+    ]
+}
+];
